8237e9bd56b2116a5edbdbf4d00bf1ff774c6772
Binh <lethanhbinh6203@gmail.com>
2023-02-11 21:04:05 +0700

    Set up docker

--- client/src/components/Fullcalendar.js
+++ client/src/components/Fullcalendar.js
@@ -2,6 +2,7 @@ import React from "react";
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import key from "../assets/key/key";
 
 export function Fullcalendar(props) {
     const scrollTime = (new Date().getHours() - 2).toString().padStart(2, '0') + ":00:00";
@@ -16,7 +17,7 @@ export function Fullcalendar(props) {
                 plugins={[ timeGridPlugin, googleCalendarPlugin ]}
                 ref={props.calendarRef}
                 initialView="timeGridWeek"
-                googleCalendarApiKey='AIzaSyBIhX1U9MrmqfpYT_GDGTcS6cEZh3jzpDY'
+                googleCalendarApiKey={key.googleCalendarApiKey}
                 nowIndicator={true}
                 allDaySlot={false}
                 scrollTime={scrollTime}
